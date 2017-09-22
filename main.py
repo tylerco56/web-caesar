c0cc919eda7243688a7c1fa8c07b05c75cbc9ac8
Tyler Langenfeld <tylerco.56@gmail.com>
2017-09-22 14:17:41 -0500

    deleted helper files and finished main

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
     <html>
         <head>
             <style>
-                form {
+                form {{
                     background-color: #eee;
                     padding: 20px;
                     margin: 0 auto;
                     width: 540px;
                     font: 16px sans-serif;
                     border-radius: 10px;
-                }
-                textarea {
+                }}
+                textarea {{
                     margin: 10px 0;
                     width: 540px;
                     height: 120px;
-                }
+                }}
             </style>
         </head>
         <body>
@@ -30,7 +30,7 @@ form = """
             <form action="/" method="POST">
                 Rotate By:
                 <input type="text" id="rot" name="rot" value="0" /><br>
-                <textarea name="text"></textarea><br>
+                <textarea name="text">{0}</textarea><br>
                 <input type="submit" value="Submit Query">
             </form>
         </body>
@@ -39,15 +39,13 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = str(request.form['text'])
     
-    return "<h1>" + rotate_string(text, rot) + "</h1>"
-
-
+    return form.format(rotate_string(text, rot))
 
 app.run()
